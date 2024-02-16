51e137c08efcaba250c9cbbe7cd5839f0be0add8
Elisa <113119301+CheesecakeTM@users.noreply.github.com>
2024-02-16 17:59:50 +0100

    pop-up aggiunto ma non animato e funzionante.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.queryselector("#pop-btn").addEventListener("click", function () {
+    document.querySelector(".frame-main .popup-container").classList.add("active");
+});
+
+document.queryselector("#pop-c-btn").addEventListener("click", function () {
+    document.querySelector(".frame-main .popup-container").classList.remove("active");
+});
