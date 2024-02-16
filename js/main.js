6d5cb8bd8777d53bdd74f4b4c9bd723cd2b237c7
Elisa <113119301+CheesecakeTM@users.noreply.github.com>
2024-02-16 19:41:41 +0100

    Pop-up completo e funyionante.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-document.queryselector("#pop-btn").addEventListener("click", function () {
-    document.querySelector(".frame-main .popup-container").classList.add("active");
-});
+document.addEventListener("DOMContentLoaded", function() {
+    const subscribeButton = document.querySelector('.subscribe');
+    const popupContainer = document.querySelector('.popup-container');
+    const popupCloseButton = document.querySelector('.popup-close-btn');
 
-document.queryselector("#pop-c-btn").addEventListener("click", function () {
-    document.querySelector(".frame-main .popup-container").classList.remove("active");
+    // Function to toggle popup-container's active state
+    function togglePopup() {
+        popupContainer.classList.toggle('active');
+    }
+
+    // Event listener for subscribe button
+    subscribeButton.addEventListener('click', togglePopup);
+
+    // Event listener for popup close button
+    popupCloseButton.addEventListener('click', togglePopup);
 });
