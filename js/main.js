4f3defd2584b0982e204ec27a0ff4f350cf27b0e
Elisa <113119301+CheesecakeTM@users.noreply.github.com>
2024-02-19 12:39:22 +0100

    Aggiunte e linkate le altre due pagine (vuote per ora).

--- js/main.js
+++ js/main.js
@@ -3,14 +3,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const popupContainer = document.querySelector('.popup-container');
     const popupCloseButton = document.querySelector('.popup-close-btn');
 
-    // Function to toggle popup-container's active state
     function togglePopup() {
         popupContainer.classList.toggle('active');
     }
 
-    // Event listener for subscribe button
     subscribeButton.addEventListener('click', togglePopup);
 
-    // Event listener for popup close button
     popupCloseButton.addEventListener('click', togglePopup);
 });
